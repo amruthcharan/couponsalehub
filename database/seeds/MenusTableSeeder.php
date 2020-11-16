a52096169d723bf3cc76b10031f977a52e53c788
Amruth Charan <amruthcharan1@gmail.com>
2020-11-17 01:40:10 +0530

    fixed seeders

--- database/seeds/MenusTableSeeder.php
+++ database/seeds/MenusTableSeeder.php
@@ -1,38 +1,23 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use TCG\Voyager\Models\Menu;
 
 class MenusTableSeeder extends Seeder
 {
-
     /**
-     * Auto generated seed file
+     * Auto generated seed file.
      *
      * @return void
      */
     public function run()
     {
-        
+        Menu::firstOrCreate([
+            'name' => 'admin',
+        ]);
 
-        \DB::table('menus')->delete();
-        
-        \DB::table('menus')->insert(array (
-            0 => 
-            array (
-                'id' => 1,
-                'name' => 'admin',
-                'created_at' => '2020-10-24 01:52:44',
-                'updated_at' => '2020-10-24 01:52:44',
-            ),
-            1 => 
-            array (
-                'id' => 2,
-                'name' => 'main-nav',
-                'created_at' => '2020-11-08 02:14:34',
-                'updated_at' => '2020-11-08 02:14:34',
-            ),
-        ));
-        
-        
+        Menu::firstOrCreate([
+            'name' => 'main-nav',
+        ]);
     }
 }
