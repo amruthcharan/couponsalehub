d4df74f32038f78ba79c1598897c58d8b35abae8
Amruth Charan <amruthcharan1@gmail.com>
2020-12-07 00:47:14 +0530

    added gtag

--- resources/views/frontend/layouts/v2.blade.php
+++ resources/views/frontend/layouts/v2.blade.php
@@ -42,13 +42,13 @@
 
 @if(setting('site.google_analytics_tracking_id'))
     <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?" . {{ setting('site.google_analytics_tracking_id') }}></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?{{ setting('site.google_analytics_tracking_id') }}"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
 
-        gtag('config', {{ setting('site.google_analytics_tracking_id') }});
+        gtag('config', '{{ setting('site.google_analytics_tracking_id') }}');
     </script>
 @endif
 </body>
