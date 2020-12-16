27c3c3e339b14b9269b565d800d3495e71284f54
Amruth Charan <amruthcharan1@gmail.com>
2020-12-16 08:33:49 +0530

    padding adjusted

--- resources/views/frontend/v2/reviews/index.blade.php
+++ resources/views/frontend/v2/reviews/index.blade.php
@@ -6,16 +6,16 @@
 @section('body')
     <div class="header">
         <div class="container-fluid">
-            <div class="row">
+            <div class="u-bg-white">
                 @include('frontend.v2.home.top-reviews')
-                <div class="col-md-12 mt-3">
+                <div class="col-md-12 py-2">
                     {!! $top->links('frontend.v2.partials.pagination') !!}
                 </div>
 
             </div>
-            <div class="row">
+            <div class="u-bg-white">
                 @include('frontend.v2.home.popular-stores')
-                <div class="col-md-12 mt-3">
+                <div class="col-md-12 py-2">
                     {!! $popular->links('frontend.v2.partials.pagination') !!}
                 </div>
             </div>
