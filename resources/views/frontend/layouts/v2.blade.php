5129b61acdc216fb8a101cad6993c825ad8380b8
Amruth Charan <amruthcharan1@gmail.com>
2021-02-06 23:36:54 +0530

    removed extra code

--- resources/views/frontend/layouts/v2.blade.php
+++ resources/views/frontend/layouts/v2.blade.php
@@ -42,23 +42,6 @@
 @yield('scripts')
 
 @if(setting('site.google_analytics_tracking_id'))
-    <!-- Tag Manager -->
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-WJTBCDM');</script>
-    <!-- End Google Tag Manager -->
-    <!-- GA 4 -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-11RW2JST9T"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-11RW2JST9T');
-    </script>
     <!-- Global site tag (gtag.js) - Google Analytics -->
 
     <script async src="https://www.googletagmanager.com/gtag/js?{{ setting('site.google_analytics_tracking_id') }}"></script>
