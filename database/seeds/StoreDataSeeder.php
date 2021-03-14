3da26411d67c401bb9b1d049a77919a2983a907a
Amruth Charan <amruthcharan1@gmail.com>
2021-03-14 23:15:09 +0530

    Fixed seeding issue

--- database/seeds/StoreDataSeeder.php
+++ database/seeds/StoreDataSeeder.php
@@ -15,23 +15,24 @@ class StoreDataSeeder extends Seeder
      */
     public function run()
     {
-        $categories = Category::take(3)->get();
-        foreach ($categories as $category) {
-            factory(Store::class, 5)
-                ->create(['category_id' => $category->id])
-                ->each(function ($store){
-                    factory(Heading::class, 3)
-                        ->create(['store_id' => $store->id])
-                        ->each(function ($heading) use($store) {
-                            factory(Coupon::class, 1)
-                                ->create(['heading_id' => $heading->id, 'store_id' => $store->id]);
-                        });
-                    factory(Coupon::class, 4)
-                        ->create(['is_editor_pick' => true, 'store_id' => $store->id]);
-                    factory(Coupon::class, 2)
-                        ->create(['is_editor_pick' => false, 'store_id' => $store->id]);
-                });
+        if (Store::count() == 0) {
+            $categories = Category::take(3)->get();
+            foreach ($categories as $category) {
+                factory(Store::class, 5)
+                    ->create(['category_id' => $category->id])
+                    ->each(function ($store){
+                        factory(Heading::class, 3)
+                            ->create(['store_id' => $store->id])
+                            ->each(function ($heading) use($store) {
+                                factory(Coupon::class, 1)
+                                    ->create(['heading_id' => $heading->id, 'store_id' => $store->id]);
+                            });
+                        factory(Coupon::class, 4)
+                            ->create(['is_editor_pick' => true, 'store_id' => $store->id]);
+                        factory(Coupon::class, 2)
+                            ->create(['is_editor_pick' => false, 'store_id' => $store->id]);
+                    });
+            }
         }
-
     }
 }
