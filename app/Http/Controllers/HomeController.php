26ad75e75eba6cf0bee9e6859906ab8af4f04ac6
Amruth Charan <amruthcharan1@gmail.com>
2021-03-23 01:45:53 +0530

    Fixed to show only 10 posts

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,10 +11,10 @@ class HomeController extends Controller
     public function __invoke()
     {
         $categories = Category::homepage()->take(8);
-        $articles = Post::published()->select('slug', 'image', 'excerpt', 'title')->latest('updated_at')->take(14)->get()->toArray();
+        $articles = Post::published()->select('slug', 'image', 'excerpt', 'title')->latest('updated_at')->take(12)->get()->toArray();
         $right = array_slice($articles,0,2);
         $slider = array_slice($articles,2, 4);
-        $posts = array_slice($articles,4, 14);
+        $posts = array_slice($articles,4, 12);
         $top = Store::published()->whereTopReview(true)->select('slug', 'feature_image', 'name')->latest('updated_at')->take(8)->get()->toArray();
         $popular = Store::published()->wherePopularStore(true)->select('slug', 'logo', 'name')->latest('updated_at')->take(12)->get()->toArray();
         return view('frontend.' . config('nextgen.theme') . '.home', compact(['right', 'slider', 'categories', 'top', 'popular', 'posts']));
