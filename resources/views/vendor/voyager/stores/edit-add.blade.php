bdf77f3b7051ece7c9a4f084477d0e109b1c69de
amruthcharan <amruthcharan1@gmail.com>
2021-07-28 21:52:31 +0530

    making category compulsory

--- resources/views/vendor/voyager/stores/edit-add.blade.php
+++ resources/views/vendor/voyager/stores/edit-add.blade.php
@@ -259,7 +259,8 @@
                             </div>
                             <div class="form-group">
                                 <label for="category_id">Category</label>
-                                <select class="form-control" name="category_id">
+                                <select class="form-control" name="category_id" required>
+                                    <option value="">Select Category</option>
                                     @foreach(Voyager::model('Category')::all() as $category)
                                         <option value="{{ $category->id }}"@if(isset($dataTypeContent->category_id) && $dataTypeContent->category_id == $category->id) selected="selected"@endif>{{ $category->name }}</option>
                                     @endforeach
